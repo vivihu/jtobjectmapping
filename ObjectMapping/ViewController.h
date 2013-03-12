c8c92abb46f94749e2e76adf3a250dc94cf061a2
black apple <wenzhou.1991@163.com>
2013-03-12 09:18:46 +0800

    Initial Commit

---
+++ ObjectMapping/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ObjectMapping
+//
+//  Created by icreative-mini on 13-3-12.
+//  Copyright (c) 2013年 icreative-mini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ObjectMapping/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ObjectMapping
+//
+//  Created by icreative-mini on 13-3-12.
+//  Copyright (c) 2013年 icreative-mini. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
